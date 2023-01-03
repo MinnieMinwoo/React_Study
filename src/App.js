ebfebb87911a3b11eb3ad1453930d9137bda0da3
MinnieMinwoo <nhs075241@gmail.com>
2023-01-03 14:38:44 +0900

    toDoList practice

--- src/App.js
+++ src/App.js
@@ -2,51 +2,33 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("Created");
-    return () => console.log("Destroyed");
-  }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDoList((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDoList.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDoList.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
     </div>
   );
-
-  {
-    /*
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log(`Boot`);
-  }, [counter]);
-  useEffect(() => {
-    console.log(`Counter`);
-  }, [counter]);
-  useEffect(() => {
-    console.log(`Search for ${keyword}`);
-  }, [keyword]);
-  useEffect(() => {
-    console.log(`keyword & counter`);
-  }, [keyword, counter]);
-  return (
-    <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>Click me</button>  
-    </div>
-  );
-  >*/
-  }
 }
 
 export default App;
