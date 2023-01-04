ab0035aa4862c2ec9d838084eb1855fefb0ba319
MinnieMinwoo <nhs075241@gmail.com>
2023-01-04 17:05:08 +0900

    fix build path

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import MovieDetail from "../components/MovieDetail";
 function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState({});
   const { id } = useParams();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    console.log(json.data.movie);
+    setMovie(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  console.log(id);
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <MovieDetail
+          key={movie.id}
+          coverImg={movie.large_cover_image}
+          title={movie.title}
+          year={movie.year}
+          rate={movie.rating}
+          genres={movie.hasOwnProperty("genres") ? movie.genres : []}
+          detail={movie.description_full}
+        ></MovieDetail>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
