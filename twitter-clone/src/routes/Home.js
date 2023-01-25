00fb0ce07c4877904c9e7b82d273e128f0fa67f9
MinnieMinwoo <nhs075241@gmail.com>
2023-01-25 12:43:00 +0900

    update

--- twitter-clone/src/routes/Home.js
+++ twitter-clone/src/routes/Home.js
@@ -1,4 +1,64 @@
-import React from "react";
+import { dbService } from "fbase";
+import { addDoc, collection, getDocs, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  const getTweets = async () => {
+    const q = query(collection(dbService, "tweets"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((document) => {
+      const tweetObj = {
+        ...document.data(),
+        id: document.id,
+      };
+
+      setTweets((prev) => [tweetObj, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getTweets();
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "tweets"), {
+        tweet,
+        createdAt: Date.now(),
+      });
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+    setTweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="tweet" />
+      </form>
+      <div>
+        {tweets.map((data) => (
+          <div key={data.id}>
+            <h4>{data.tweet}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default Home;
